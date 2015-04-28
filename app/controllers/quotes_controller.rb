ffdf197784e026f8b713ebebee3c306e4a45e47d
Michael Lee <mlee16@hawk.iit.edu>
2015-04-28 06:41:11 -0500

    created new and create view

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,5 +2,19 @@ class QuotesController < ApplicationController
 
 	def index
 	end
-	
+
+	def new
+		@quote = Quote.new
+	end
+
+	def create
+		@quote = Quote.new(quote_params)
+	end
+
+	private
+
+	def quote_params
+		params.require(:quote).permit(:name, :description)
+	end
+
 end
