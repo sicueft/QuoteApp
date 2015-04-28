28d164a10070a4cdc879f277e66320b96778f776
Michael Lee <mlee16@hawk.iit.edu>
2015-04-28 15:37:35 -0500

    Added styling to the application

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :body, :quote_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428163607) do
+ActiveRecord::Schema.define(version: 20150428193735) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "quote_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "name"
