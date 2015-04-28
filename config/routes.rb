ad680568b3893d1be9143afd33ca0f75ae664968
Michael Lee <mlee16@hawk.iit.edu>
2015-04-28 11:03:05 -0500

    Added signed out feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :quotes
 
   get '/auth/:provider/callback', to: 'sessions#create'
-
+  get '/signout', to: 'sessions#destroy', :as => :signout
   root "quotes#index"
 end
