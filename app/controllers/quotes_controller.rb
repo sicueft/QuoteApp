444e56342e94573e706545e569b5ce07025db9c7
Michael Lee <mlee16@hawk.iit.edu>
2015-04-28 07:21:11 -0500

    Added show view and fixed before_action to make the submission appear

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,10 @@
 class QuotesController < ApplicationController
+	before_action :find_quote, only: [:show, :edit, :update, :destroy]
 
 	def index
+	end
+
+	def show
 	end
 
 	def new
@@ -9,12 +13,22 @@ class QuotesController < ApplicationController
 
 	def create
 		@quote = Quote.new(quote_params)
+
+		if @quote.save
+			redirect_to @quote, notice: "Quote was successfully created."
+		else
+			render 'new'
+		end
 	end
 
 	private
 
 	def quote_params
 		params.require(:quote).permit(:name, :description)
+	end
+
+	def find_quote
+		@quote = Quote.find(params[:id])
 	end
 
 end
